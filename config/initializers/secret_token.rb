117881d6cab41b807703f9a6766f227e930a8a89
Yasmine Gaber <yasmine.gaber@espace.com.eg>
2012-05-09 13:31:55 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1f8a44d241bc259925b0455201ce8c7901a03a06dfa9b5d9c490feb4eadc1e794fc8c83016ba06bfca3f6c9e6aecfc07382126a924e22e1862fa16b5b3d675f9'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'Home') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Help' }
+  end
+
+  describe "About page" do
+    before { visit help_path }
+
+#    it { should have_selector('h1',    text: 'About Us') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| About' }
+  end
+
+  describe "Contact Us Page" do
+    before { visit help_path }
+
+ #   it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Contact' }
+  end
+	
+end
+
